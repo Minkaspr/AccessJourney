18ebb3291af180cf4d3098860eab187e834a6dbc
Dario <106852390+Minkaspr@users.noreply.github.com>
2024-04-15 19:05:02 -0500

    initial backend project

---
+++ accessjourney-be/src/main/java/com/mk/accessjourneybe/AccessjourneyBeApplication.java
@@ -0,0 +1,13 @@
+package com.mk.accessjourneybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessjourneyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessjourneyBeApplication.class, args);
+	}
+
+}
---
+++ accessjourney-be/src/test/java/com/mk/accessjourneybe/AccessjourneyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk.accessjourneybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessjourneyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
